115c2432bbe915407c6c2b1242823d8e7f93dff4
pkhurana-whjr <pkhurana.asr@gmail.com>
2021-04-27 16:07:20 +0530

    numbers changed

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20);
-    platform=new Ground(300, 400, 400, 400);
+    platform=new Ground(400, 400, 500, 500);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
